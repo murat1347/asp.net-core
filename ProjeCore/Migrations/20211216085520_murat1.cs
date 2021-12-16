1a2a92eb122e4c801d0468db3630edef20a440bd
Murat Çiçek <murat1347@yandex.com>
2021-12-16 15:48:22 +0300

    final

---
+++ ProjeCore/Migrations/20211216085520_murat1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjeCore.Migrations
+{
+    public partial class murat1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Birims",
+                columns: table => new
+                {
+                    BirimId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BirimAd = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Birims", x => x.BirimId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personels",
+                columns: table => new
+                {
+                    PersonelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Soyad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sehir = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirimID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personels", x => x.PersonelId);
+                    table.ForeignKey(
+                        name: "FK_Personels_Birims_BirimID",
+                        column: x => x.BirimID,
+                        principalTable: "Birims",
+                        principalColumn: "BirimId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personels_BirimID",
+                table: "Personels",
+                column: "BirimID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personels");
+
+            migrationBuilder.DropTable(
+                name: "Birims");
+        }
+    }
+}
